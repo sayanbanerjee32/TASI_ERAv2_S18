48315517656254c29d23d723c8069f36ec5cb306
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-06-07 13:36:13 +0530

    lr update

--- config_file.py
+++ config_file.py
@@ -4,8 +4,8 @@ def get_config():
     return {
         "batch_size": 32,
         "num_epochs": 18,
-        # "lr": 10**-4,
-        "lr": 1e-4/10,
+        "lr": 10**-4,
+        # "lr": 1e-4/10,
         "seq_len": 350,
         "d_model": 512,
         "lang_src": "en",
@@ -21,4 +21,3 @@ def get_weights_file_path(config, epoch: str):
     model_basename = config["model_basename"]
     model_filename = f"{model_basename}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
-    
